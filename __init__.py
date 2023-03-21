064e8edd17e00fd7786a8629258a3ae5c903bfd5
Asen Asenov <47159032+AsenAsenov1@users.noreply.github.com>
2023-03-21 19:13:03 +0200

    initial status

--- __init__.py
+++ __init__.py
@@ -5,9 +5,7 @@ app.config['SECRET_KEY'] = "secretkey123"
 
 @app.route("/")  # this sets the route to this page
 def home():
-	return jsonify({'Message': "Testing"
-			'Status': "DOWN ( NOT REALLY :) )"
-})
+	return jsonify({'Message': "Testing"})
 
 if __name__ == "__main__":
     app.run()

8206551bad0907ce23c18ae76f392050876e7429
Asen Asenov <47159032+AsenAsenov1@users.noreply.github.com>
2023-03-22 22:48:28 +0200

    update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,9 +3,16 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secretkey123"
 
-@app.route("/")  # this sets the route to this page
+
+@app.route("/")
 def home():
-	return jsonify({'Message': "None"})
+    return jsonify({'Message': "None"})
+
+
+@app.route("/main")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
 
 if __name__ == "__main__":
     app.run()
